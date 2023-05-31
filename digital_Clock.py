8c616bdb9e59bdde3529457ff72ee4e6395c375c
chandan-srinivas <srinivaschandan0@gmail.com>
2023-05-31 15:18:30 +0530

    abc

---
+++ digital_Clock.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Digital Clock")
+
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text= string)
+    label.after(1000, time)
+
+
+label = Label(root,font = ("ds-digital",70),background = "black",foreground = "red")
+label.pack(anchor = 'center')
+time()
+
+mainloop()
